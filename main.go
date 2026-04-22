10f6ac3adf583a55f7e51f1d09b18c5ab5c6c557
takicode <takipemmanuel@gmail.com>
2026-04-22 06:55:10 +0100

    getPid ongoing

--- main.go
+++ main.go
@@ -4,9 +4,43 @@ package main
 import (
 	"fmt"
 	"os"
+
 )
+
+// func getPids()([]int, error){
+//    entries, err:= os.ReadDir("/proc")
+// 	if err != nil{
+// 		return nil, err
+// 	}
+
+// 	for _, entry:= range entries{
+// 			if entry.IsDir(){
+// 				fmt.Print(entry.Name())
+// 			}
+// 	}
+// }
 
 
 func main(){
-	pids, err:= 
+	// pids, err := getPids()
+  // if err != nil{
+	// 	fmt.Println("Error:", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(pids)
+
+	  entries, _:= os.ReadDir("/proc")
+	// if err != nil{
+	// 	return nil, err
+	// }
+
+	for _, entry:= range entries{
+			if entry.IsDir(){
+				pid, err := strconv.Atoi(entry.name())
+				if err != nil{
+					
+				}
+			}
+	}
 }
